2fe4420fafa238f4ce38eb8590b68c329100831d
YeabsiraAddis19 <yeabsira23addis@gmail.com>
2026-04-02 12:41:21 +0300

    feat: add Spring Data JPA repositories with derived queries

---
+++ src/main/java/com/shopwave/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+// Student Number: ATE/8574/14
+package com.shopwave.repository;
+
+import com.shopwave.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
+
